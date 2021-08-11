3e4e29968dd44539d99385098b4dccf64eee3c96
VacuousRom <38498439+VacuousRom@users.noreply.github.com>
2021-08-11 19:30:03 +0200

    Add files via upload

---
+++ production/src/components/infoComponent.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="info">
+      <p>{{message}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['message']
+
+}
+</script>
+
+<style scoped>
+    .info {
+        display: grid;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -100%);
+        background: #FFF9EB;
+        text-align: center;
+        width: 70%;
+        border-radius: 10px;
+        padding: 2rem;
+        box-shadow: 20px 20px 40px #2E383826;   
+
+    }
+
+</style>
